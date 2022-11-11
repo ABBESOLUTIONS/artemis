14cc31256eb96a2051473833db0c088e404b8648
ABBEALLSOLUTIONS <abbeallsolutions@gmail.com>
2022-11-11 14:55:01 +0000

    Contact Form fixed

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,9 @@
 import { Container, styled } from '@mui/material';
 import React from 'react';
 import AboutDrawer from '../components/AboutDrawer';
+import HeroComponent from '../components/HeroComponent';
 import ContactElements from '../sections/ContactPage.tsx/ContactElements';
+import ContactForm from '../sections/ContactPage.tsx/ContactForm';
 
 const ContactStyle= styled("div")(()=>({
     
@@ -10,7 +12,9 @@ const ContactStyle= styled("div")(()=>({
 function Contact() {
     return (
        <ContactStyle>
+        <HeroComponent/>
             <ContactElements/>
+            <ContactForm/>
        </ContactStyle>
     );
 }
---
+++ src/sections/ContactPage.tsx/ContactForm.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Box, Button, Card, Container, FormControl, Grid, Input, InputLabel, Stack, styled, TextField, Typography, OutlinedInput, FormControlLabel, Checkbox } from '@mui/material';
+import SectionStyle from '../../styles/SectionStyle';
+import ArrowForwardIos from '@mui/icons-material/ArrowForwardIos';
+import { PROJECT_COLORS } from '../../common/colors';
+
+const ContactElementsContainer=styled(SectionStyle)(()=>({
+    height:"600px",
+    display:"flex",
+    flexDirection:"column",
+    alignItems:"center",
+    justifyContent:"center",
+}));
+
+function ContactForm() {
+    return (
+        <ContactElementsContainer>
+            <Typography variant="h6" sx={{textAlign:"center", fontSize:'', color:PROJECT_COLORS.primarySwatch}}>Nous envoyer un message</Typography>
+            <Typography variant='h3' sx={{ fontWeight:"bold", fontSize:"45px", marginBottom:"50px", textAlign:"center" }}>Veuillez renseigner le formulaire</Typography>
+            <Box component={"form"} sx={{'& .MuiTextField-root': { m:2, width: '50ch' },}}>
+                <div>
+                <TextField required id="outlined-required" label="Required" defaultValue="Hello World"/>
+                <TextField required id="outlined-disabled" label="Disabled" defaultValue="Hello World"/>
+                </div>
+                <div>
+                <TextField required id="outlined-required" label="Required" defaultValue="Hello World"/>
+                <TextField required id="outlined-disabled" label="Disabled" defaultValue="Hello World"/>
+                </div>
+                <div>
+                <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+                {/* <TextField variant="standard" label={"Comment pouvons nous vous aidé"} /> */}
+
+                    <InputLabel htmlFor="standard-adornment-amount" >Amount</InputLabel>
+                    <OutlinedInput
+                        id="standard-adornment-amount"
+                    />
+                   
+        <Typography sx={{margin:"10px"}}>
+                Selectionner tous vos documents à envoyer
+            </Typography>
+            <OutlinedInput type='file' id="upload"></OutlinedInput>
+            <FormControlLabel
+          value="end"
+          control={<Checkbox />}
+          label="J'accepte les termes et conditions"
+          labelPlacement="end"
+        />
+            </FormControl>
+                </div>
+            </Box>
+                <Button startIcon={<ArrowForwardIos/>} sx={{color:"white", backgroundColor:"#138F82", padding:"18px"}}> Envoyer </Button>
+        </ContactElementsContainer>
+    );
+}
+
+export default ContactForm;
