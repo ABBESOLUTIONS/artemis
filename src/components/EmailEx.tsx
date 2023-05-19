3b8f5803972caa371f2e39454ce5ca82622d083c
kaoKodinart <h.kao@kodinart.com>
2023-05-19 14:41:06 +0200

    mailing start

---
+++ src/components/EmailEx.tsx
@@ -0,0 +1,17 @@
+import { render } from '@react-email/render';
+import React from 'react';
+import ServiceOverviewCard from './ServiceOverviewCard';
+import { Html } from '@react-email/html';
+import { Button } from '@react-email/button';
+// import { Email, Box, Item, A, Span } from 'react-email-render';
+
+
+function EmailEx() {
+    return (
+        <Html>
+            <Button>Marche§§§§</Button>
+        </Html>
+    );
+}
+
+export default EmailEx;
